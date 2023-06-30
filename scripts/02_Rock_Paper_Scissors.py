88b1c19d42467f22ac946b419cceb11d19ed8ff2
Kathryn Potten <137423893+kathrynpotten@users.noreply.github.com>
2023-06-30 18:08:46 +0100

    2-1 Rock Paper Scissors

--- scripts/02_Rock_Paper_Scissors.py
+++ scripts/02_Rock_Paper_Scissors.py
@@ -14,10 +14,28 @@ loss = 0
 draw = 3
 win = 6         """
 
-scores = {'A':1,'B':2,'C':3, 'X':1,'Y':2,'Z':3}
+scores = {'A':1,'B':2,'C':3,
+          'X':1,'Y':2,'Z':3}
+outcomes = {'win':6,'loss':0,'draw':3}
 
-def convert_to_score(input):
-    return [scores.get(val) for val in input]
+
+def convert_to_score(round):
+    return [scores.get(val) for val in round]
+
+def outcome(round_val):
+    opponent, player = round_val[0], round_val[1]
+    if player > opponent:
+        outcome = 'win'
+    elif player < opponent:
+        outcome = 'loss'
+    elif player == opponent:
+        outcome = 'draw'
+    return outcome
+
+def score(round_val):
+    opponent, player = round_val[0], round_val[1]
+    return player + outcomes.get(outcome(round_val))
+
 
 def select_round(rounds):
     # split into separate rounds
@@ -28,22 +46,24 @@ def select_round(rounds):
         rounds.append(round)
     return rounds
 
+
 def total_score(strategy_guide):
     # split into separate rounds
     rounds = select_round(strategy_guide)
-    # convert letters into scores
-    scores_list = [convert_to_score(round) for round in rounds]
-    # calculate total score
-    total_score = 0
-    for round in scores_list:
-        if round[1] > round[0]:
-            total_score += 6 + round[1]
-        elif round[1] == round[0]:
-            total_score += 3 + round[1]
-        elif round[1] < round[0]:
-            total_score += 0 + round[1]
-    return total_score
+    converted = [convert_to_score(round) for round in rounds]
+    # calculate score per round
+    scores = [score(round) for round in converted]
+    return sum(scores)
+
 
 assert total_score(test_data) == test_result
+
+with open('C:/Users/Kathryn/OneDrive/dev/AdventOfCode2022/input_data/02_Rock_Paper_Scissors.txt', 'r', encoding="utf-8") as file:
+    input = file.read()
+
+answer_1 = total_score(input)
+print(answer_1)
+
+
 
 
