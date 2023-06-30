494651faf067309cbcdf4fb58102579714a94405
Kathryn Potten <137423893+kathrynpotten@users.noreply.github.com>
2023-06-30 16:43:08 +0100

    1-1, 1-2 clean-up

--- scripts/01_Calorie_Counting.py
+++ scripts/01_Calorie_Counting.py
@@ -17,20 +17,18 @@ test_data = """
 test_result = 24000
 test_result2 = 45000
 
+def calories_per_elf(elf):
+    return sum(int(cals) for cals in elf.strip().split('\n'))
 
 def most_calories(data):
-    # split data into elves and convert into integers
-    calories = data.strip().split('\n') 
-    calories = [0 if cals == '' else int(cals) for cals in calories]
-    #calculate total calories per elf and keep max
+    # calculate total calories per elf
+    elves = data.strip().split('\n\n') 
+    calories = [calories_per_elf(elf) for elf in elves]
+    #keep max
     max_calories = 0	
-    for i in range(calories.count(0)):
-        total_calories_carried = sum(calories[:calories.index(0)+1])
-        calories = calories[calories.index(0)+1:]
+    for i in range(len(calories)):
+        total_calories_carried = calories[i]
         if total_calories_carried > max_calories:
-            max_calories = total_calories_carried
-    total_calories_carried = sum(calories)
-    if total_calories_carried > max_calories:
             max_calories = total_calories_carried
     return max_calories
 
@@ -45,34 +43,23 @@ test_data_new = """
 
 assert most_calories(test_data_new) == 10000
 
-with open('./input_data/01_Calorie_Counting.txt', 'r', encoding="utf-8") as file:
-    input = file.read()
-    
-print(most_calories(input))
-
-
-def calories_per_elf(data):
-    calories = data.strip().split('\n') 
-    calories = [0 if cals == '' else int(cals) for cals in calories]
-    #calculate total calories per elf
-    calories_per_elf = []
-    for i in range(calories.count(0)):
-        total_calories_carried = sum(calories[:calories.index(0)+1])
-        calories = calories[calories.index(0)+1:]
-        calories_per_elf.append(total_calories_carried)
-    total_calories_carried_final = sum(calories)
-    calories_per_elf.append(total_calories_carried_final)
-    return calories_per_elf
 
 def calories_top_three(data):
-    calories = calories_per_elf(data)
+    elves = data.strip().split('\n\n')
+    calories = [calories_per_elf(elf) for elf in elves]
     calories.sort(reverse=True)
     return sum(calories[:3])
 
 
 assert calories_top_three(test_data) == test_result2
 
-print(calories_top_three(input))
+with open('./input_data/01_Calorie_Counting.txt', 'r', encoding="utf-8") as file:
+    input = file.read()
+
+answer_1 = most_calories(input)   
+answer_2 = calories_top_three(input)
+
+print(answer_1, answer_2)
     
     
 
