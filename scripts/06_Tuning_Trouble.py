b038e8f8ec28bda31176cb4ee91286d0fdae4620
Kathryn Potten <137423893+kathrynpotten@users.noreply.github.com>
2023-07-14 16:24:26 +0100

    6-2 Tuning Trouble

--- scripts/06_Tuning_Trouble.py
+++ scripts/06_Tuning_Trouble.py
@@ -10,48 +10,64 @@ test_result_3 = 6
 test_result_4 = 10
 test_result_5 = 11
 
+test_result2_1 = 19
+test_result2_2 = 23
+test_result2_3 = 23
+test_result2_4 = 29
+test_result2_5 = 26
 
 
-def check_duplicates(chars):
-    """ for a set of 4 characters, check if any repeats found within"""
-    if len(set(chars)) == 4:
+
+def check_duplicates(chars, number):
+    """ for a set of characters of given length, check if any repeats found within"""
+    if len(set(chars)) == number:
         return False
     else:
         return True
     
-assert check_duplicates('mjqj') == True
-assert check_duplicates('jpqm') == False
+assert check_duplicates('mjqj',4) == True
+assert check_duplicates('jpqm',4) == False
  
-def split_signal(signal):
-    """ split signal into sets of 4 characters """
-    return [signal[i:i+4] for i in range(len(signal)-3)]
+def split_signal(signal,number):
+    """ split signal into sets of characters of given length """
+    return [signal[i:i+number] for i in range(len(signal)-3)]
 
-assert split_signal('mjqjpqmg') == ['mjqj','jqjp','qjpq','jpqm','pqmg']
+assert split_signal('mjqjpqmg',4) == ['mjqj','jqjp','qjpq','jpqm','pqmg']
 
-def start_of_packet_marker(signal):
-    """ find and return start-of-packet marker """
-    chars = split_signal(signal)
+def marker(signal,number):
+    """ find and return end character of marker of given length """
+    chars = split_signal(signal,number)
     for char_str,index in zip(chars,range(0,len(chars))):
-        if check_duplicates(char_str) == False:
-            return index+4
+        if check_duplicates(char_str,number) == False:
+            return index+number
         else:
             continue
     return False
 
-assert start_of_packet_marker('mjqj') == False
-assert start_of_packet_marker('jpqm') == 4
+assert marker('mjqj',4) == False
+assert marker('jpqm',4) == 4
 
 
 
-assert start_of_packet_marker(test_data_1) == test_result_1
-assert start_of_packet_marker(test_data_2) == test_result_2
-assert start_of_packet_marker(test_data_3) == test_result_3
-assert start_of_packet_marker(test_data_4) == test_result_4
-assert start_of_packet_marker(test_data_5) == test_result_5
+assert marker(test_data_1,4) == test_result_1
+assert marker(test_data_2,4) == test_result_2
+assert marker(test_data_3,4) == test_result_3
+assert marker(test_data_4,4) == test_result_4
+assert marker(test_data_5,4) == test_result_5
 
+
+
+assert marker(test_data_1,14) == test_result2_1
+assert marker(test_data_2,14) == test_result2_2
+assert marker(test_data_3,14) == test_result2_3
+assert marker(test_data_4,14) == test_result2_4
+assert marker(test_data_5,14) == test_result2_5
 
 with open('../input_data/06_Tuning_Trouble.txt', 'r', encoding="utf-8") as file:
     input = file.read()
 
-answer_1 = start_of_packet_marker(input)
+answer_1 = marker(input,4)
 print(answer_1)    
+
+answer_2 = marker(input,14)
+print(answer_2)
