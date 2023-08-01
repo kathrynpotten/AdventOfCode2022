517c051dd0e71b9fb3bb19cc4a182da92bdcca02
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-08-01 17:26:02 +0100

    8-2 Treetop Tree House

--- scripts/08_Treetop_Tree_House.py
+++ scripts/08_Treetop_Tree_House.py
@@ -1,3 +1,7 @@
+import re
+import numpy as np
+
+
 test_data = """30373
 25512
 65332
@@ -8,55 +12,47 @@ test_result = 21
 test_result_2 = 8
 
 
-
-import numpy as np
-import re
-
 test_array = np.array([list(map(int, re.findall(r"[0-9]", line))) for line in test_data])
 
 
 
-#check row from RHS
+
 def visible_from_right(row):
+    """ check row from RHS """
     visible = []
     for i in range(len(row)):
         if any(tree >= row[i] for tree in row[i+1:]):
             continue
-        else:
-            visible.append(i)
+        visible.append(i)
     return len(visible)    
-
-
 
 assert visible_from_right([3,0,3,7,3]) == 2
 
 
 
-#check row from LHS
+
 def visible_from_left(row):
+    """ check row from LHS """
     visible = []
     row.reverse()
     for i in range(len(row)):
         if any(tree >= row[i] for tree in row[i+1:]):
             continue
-        else:
-            visible.append(i)
+        visible.append(i)
     return len(visible)    
-
-
 
 assert visible_from_left([3,0,3,7,3]) == 2
 
 
 
 
-def is_visible(list):
+def is_visible(line):
+    """ check if tree is visible """
     visible = []
-    for i in range(len(list)):
-        if any(tree >= list[i] for tree in list[i+1:]):
+    for i in range(len(line)):
+        if any(tree >= line[i] for tree in line[i+1:]):
             continue
-        else:
-            visible.append(i)
+        visible.append(i)
     return visible   
 
 
@@ -67,13 +63,13 @@ def check_patch(array):
 
     #add any visible inner trees
     for index, row in enumerate(array):
-        row_vis_RHS = is_visible(row)
-        for tree in row_vis_RHS:
+        row_vis_rhs = is_visible(row)
+        for tree in row_vis_rhs:
             visible.append((index,tree))
         
-        row_vis_LHS = is_visible(row[::-1])
-        row_vis_LHS = [len(row)-1-tree for tree in row_vis_LHS]
-        for tree in row_vis_LHS:
+        row_vis_lhs = is_visible(row[::-1])
+        row_vis_lhs = [len(row)-1-tree for tree in row_vis_lhs]
+        for tree in row_vis_lhs:
             visible.append((index,tree))
 
     for index, column in enumerate(array.T):
@@ -93,15 +89,15 @@ def check_patch(array):
 
 assert list(test_array.T[0]) == [3,2,6,3,3]
 
-#assert check_patch(test_array)[1] == test_result
+assert check_patch(test_array)[1] == test_result
 
 
 
 with open('../input_data/08_Treetop_Tree_House.txt', 'r', encoding="utf-8") as file:
-    input = file.read().strip().split('\n')
+    input_file = file.read().strip().split('\n')
 
 
-input_array = np.array([list(map(int, re.findall(r"[0-9]", line))) for line in input])
+input_array = np.array([list(map(int, re.findall(r"[0-9]", line))) for line in input_file])
 
 
 answer_1 = check_patch(input_array)[1]
@@ -112,12 +108,13 @@ print(answer_1)
 
 """ Part 2 """
 
-def scenic(list):
+def scenic(line):
+    """ check how many trees are visible from current posiiton """
     visible = []
     count = 0
-    for i in range(1,len(list)):
+    for i in range(1,len(line)):
         count +=1
-        if list[i] < list[0]:
+        if line[i] < line[0]:
             visible.append(i)
         else:
             visible.append(i)
@@ -125,19 +122,21 @@ def scenic(list):
     return len(visible)
 
 def scenic_score(tree, array):
+    """ calculate scenic score from tree of choice """
     i, j = tree
-    scenic_score = 1
-    scenic_score *= scenic(np.flip(array[i][:j+1]))
-    scenic_score *= scenic(array[i][j:])
-    scenic_score *= scenic(array.T[j][i:])
-    scenic_score *= scenic(np.flip(array.T[j][:i+1]))
-    return scenic_score
+    score = 1
+    score *= scenic(np.flip(array[i][:j+1]))
+    score *= scenic(array[i][j:])
+    score *= scenic(array.T[j][i:])
+    score *= scenic(np.flip(array.T[j][:i+1]))
+    return score
 
 assert scenic_score((1,2), test_array) == 4
 assert scenic_score((3,2), test_array) == 8
 
 
 def max_scenic_score(array):
+    """ calculate maximum scenic score in given tree patch """
     rows, columns = np.shape(array)
     scores = []
     for i in range(rows):
