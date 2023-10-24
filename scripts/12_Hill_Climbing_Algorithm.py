9d3c04f88ed8d61904950d88d50a6d7c8e40dd5e
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-10-24 17:46:34 +0100

    12 -1 Hill Climbing Algorithm

--- scripts/12_Hill_Climbing_Algorithm.py
+++ scripts/12_Hill_Climbing_Algorithm.py
@@ -23,20 +23,23 @@ class Path:
         self.steps_taken = 0
 
     def __repr__(self):
+        # grid_repr = self.grid[1:-1, 1:-1]
         return "\n".join("".join(row) for row in self.grid)
 
     def elevation_convert(self):
-        self.converted_grid = np.copy(self.grid)
-        for index, elem in np.ndenumerate(self.grid):
+        init_grid = np.copy(self.grid)
+        converted_grid = np.char.replace(init_grid, "S", "a")
+        self.converted_grid = np.char.replace(converted_grid, "E", "z").astype("U25")
+        for index, elem in np.ndenumerate(self.converted_grid):
             self.converted_grid[index] = ord(elem) - 96
         return self.converted_grid.astype(int)
 
     def parse_heightmap(self, input):
         locations = [list(loc) for loc in input]
-        init_grid = np.array(locations, dtype="U25")
-        self.grid = np.pad(init_grid, ((1, 1), (1, 1)), constant_values="~")
+        self.grid = np.array(locations, dtype="U25")
         self.converted_grid = self.elevation_convert()
         self.current_loc = self.starting_loc()
+        self.best_signal_loc = self.best_signal_location()
         return self.converted_grid
 
     def starting_loc(self):
@@ -53,40 +56,45 @@ class Path:
         self.best_signal_loc = (row, col)
         return self.best_signal_loc
 
-    def initial_move(self, down=True, right=False, left=False, up=False):
-        if down:
-            self.grid[self.current_loc] = "v"
-            self.current_loc = tuple(x + y for x, y in zip(self.current_loc, (1, 0)))
-        elif right:
-            self.grid[self.current_loc] = ">"
-            self.current_loc = tuple(x + y for x, y in zip(self.current_loc, (0, 1)))
-        elif left:
-            self.grid[self.current_loc] = "<"
-            self.current_loc = tuple(x + y for x, y in zip(self.current_loc, (0, -1)))
-        elif up:
-            self.grid[self.current_loc] = "^"
-            self.current_loc = tuple(x + y for x, y in zip(self.current_loc, (-1, 0)))
-
     def take_step(self):
         row, col = self.current_loc
         possible_moves = []
         max_elevation = self.converted_grid[row, col] + 1
 
-        if self.converted_grid[row + 1, col] <= max_elevation:
-            step_size = self.converted_grid[row + 1, col] - max_elevation
-            possible_moves.append([(1, 0), "v", step_size])
+        def check_on_grid(row, col):
+            max_row, max_col = np.shape(self.grid)
+            if 0 <= row < max_row and 0 <= col < max_col:
+                return True
+            else:
+                return False
 
-        if self.converted_grid[row, col + 1] <= max_elevation:
-            step_size = self.converted_grid[row, col + 1] - max_elevation
-            possible_moves.append([(0, 1), ">", step_size])
+        if (
+            check_on_grid(row - 1, col)
+            and self.converted_grid[row - 1, col] <= max_elevation
+        ):
+            step_size = self.converted_grid[row - 1, col] - max_elevation
+            possible_moves.append([(-1, 0), "^", step_size])
 
-        if self.converted_grid[row, col - 1] <= max_elevation:
+        if (
+            check_on_grid(row, col - 1)
+            and self.converted_grid[row, col - 1] <= max_elevation
+        ):
             step_size = self.converted_grid[row, col - 1] - max_elevation
             possible_moves.append([(0, -1), "<", step_size])
 
-        if self.converted_grid[row - 1, col] <= max_elevation:
-            step_size = self.converted_grid[row - 1, col] - max_elevation
-            possible_moves.append([(-1, 0), "^", step_size])
+        if (
+            check_on_grid(row, col + 1)
+            and self.converted_grid[row, col + 1] <= max_elevation
+        ):
+            step_size = self.converted_grid[row, col + 1] - max_elevation
+            possible_moves.append([(0, 1), ">", step_size])
+
+        if (
+            check_on_grid(row + 1, col)
+            and self.converted_grid[row + 1, col] <= max_elevation
+        ):
+            step_size = self.converted_grid[row + 1, col] - max_elevation
+            possible_moves.append([(1, 0), "v", step_size])
 
         possible_moves.sort(key=lambda move: move[-1])
         best_move = possible_moves[-1]
@@ -98,8 +106,8 @@ class Path:
         self.grid[self.current_loc] = "S"
 
     def climb_to_best_signal_loc(self):
-        self.best_signal_loc = self.best_signal_location()
         while self.current_loc != self.best_signal_loc:
+            print(self.steps_taken)
             self.take_step()
             self.steps_taken += 1
 
@@ -113,14 +121,22 @@ class Path:
 
 test_path = Path()
 test_path.parse_heightmap(test_data)
-print(test_path)
-# print(test_path.converted_grid)
 
-print("\n")
-test_path.initial_move()
-print(test_path)
-print("\n")
-for _ in range(10):
-    test_path.take_step()
-    print(test_path)
-    print("\n")
+# steps_taken = test_path.climb_to_best_signal_loc()
+
+# print(test_path)
+
+# assert steps_taken == test_result
+
+with open(
+    "../input_data/12_Hill_Climbing_Algorithm.txt", "r", encoding="utf-8"
+) as file:
+    input = file.read().strip().split("\n")
+
+
+answer_path = Path()
+answer_path.parse_heightmap(input)
+
+
+# answer_1 = answer_path.climb_to_best_signal_loc()
+# print(answer_1)
